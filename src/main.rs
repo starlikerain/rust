6904c53115195cde2ae5e9a54efb02e98f2b86ea
StarLikeRain <pengyaokang@qq.com>
2021-07-20 11:17:28 +0800

    feat(Guess number): complete edition

--- src/main.rs
+++ src/main.rs
@@ -13,15 +13,45 @@
 // }
 //
 
+use std::cmp::Ordering;
 // ------------------------分割线------------------------
 use std::io;
 
+// prelude  // trait
+use rand::Rng;
+
 fn main() {
-    println!("guess a number game");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    let mut guess = String::new();
+    println!("secret_number: {}", secret_number);
 
-    io::stdin().read_line(&mut guess).expect("输入错误");
 
-    println!("the number u guess is {}", guess);
+    loop {
+        println!("Guess a number:");
+
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess).expect("输入错误");
+
+        // 这个写法虽然捕获了但是也退出（崩溃）了
+        // let guess:u32 = guess.trim().parse().expect("please enter a number");
+
+        // This way is better
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!!!");
+                break;
+            }
+        }
+    }
 }
+
+
+
