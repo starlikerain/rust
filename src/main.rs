790d211a1d672195f9b935ef5f86316ba7b38ea5
StarLikeRain <pengyaokang@qq.com>
2021-07-28 16:56:06 +0800

    第六章：常用的集合- 妙啊，基于现有的 V 来更新 V

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     let mut map = HashMap::new();
 
     for world in text.split_whitespace() {
+        // 基于现有的 V 来更新 V
         let count = map.entry(world).or_insert(0); // or_insert 返回的是可变引用 &mut i32 类型
         *count += 1;
     }
